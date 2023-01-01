bf280c6fa07fe6a2806387592adcba8e2959b274
MiraNizam <82273561+MiraNizam@users.noreply.github.com>
2023-01-01 21:06:01 +0300

    Download books from tululu.org.

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import requests
+from pathlib import Path
 
-url = "https://tululu.org/txt.php?id=32168"
-response = requests.get(url)
-response.raise_for_status()
+Path("books/").mkdir(parents=True, exist_ok=True)
+for i in range(1, 11):
+    url_template = f"https://tululu.org/txt.php?id={i}"
+    response = requests.get(url_template)
+    response.raise_for_status()
 
-filename = "Пески Марса. Кларк Артур"
-with open(filename, "w+", encoding="utf-8") as file:
-    book = file.write(response.text)
-
-new_book = book.replace("\xa0", " ")
-
+    filename = f"id{i}.txt"
+    with open(f"books/{filename}", "w+", encoding="utf-8") as file:
+        file.write(response.text)
 
