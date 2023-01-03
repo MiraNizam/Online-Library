03a675f971fbc08b05b86d2266995ef2380a2167
MiraNizam <82273561+MiraNizam@users.noreply.github.com>
2023-01-03 18:57:01 +0300

    Add checking for status

--- parsing_book_name.py
+++ parsing_book_name.py
@@ -6,6 +6,7 @@ def parse_book_name(book_id: int) -> str:
     """Function collects the name of books from the site."""
     url = f"https://tululu.org/b{book_id}"
     response = requests.get(url)
+    response.raise_for_status()
     soup = BeautifulSoup(response.text, 'lxml')
     title_tag = soup.find("h1")
     title_text = list(map(lambda x: x.strip(), (title_tag.text).split("::")))
