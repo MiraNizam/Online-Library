bc2b4ccfb889803b05cdfdb865356576cb97beab
MiraNizam <82273561+MiraNizam@users.noreply.github.com>
2023-01-04 20:09:07 +0300

    Delete try/except block

--- download_image.py
+++ download_image.py
@@ -1,7 +1,6 @@
 import os.path
 from pathlib import Path
 import requests
-from check_for_redirect import check_for_redirect
 from urllib.parse import urlsplit, unquote
 
 
@@ -16,13 +15,8 @@ def download_image(url, folder='images/'):
     Path(folder).mkdir(parents=True, exist_ok=True)
     filename = urlsplit(unquote(url)).path.split("/")[-1]
     picture_path = os.path.join(folder, filename)
-    print(picture_path)
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        check_for_redirect(response)
-        with open(picture_path, "wb") as file:
-            file.write(response.content)
-    except requests.HTTPError:
-        pass
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(picture_path, "wb") as file:
+        file.write(response.content)
     return picture_path
