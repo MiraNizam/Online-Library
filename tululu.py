6f9df673f1d9516919fa357486d652e2398d83e5
MiraNizam <82273561+MiraNizam@users.noreply.github.com>
2023-01-04 15:25:52 +0300

    Parse books genres

--- tululu.py
+++ tululu.py
@@ -3,29 +3,30 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
 
-def parse_page(start: int, stop: int) -> str:
-    for id in range(start, stop):
-        url = f"https://tululu.org/b{id}"
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'lxml')
-        book_details_tag = soup.find("h1")
-        book_details = list(map(lambda x: x.strip(), book_details_tag.text.split("::")))
-        picture_tag = soup.find("div", class_="bookimage").find("img")["src"]
-        picture_url = urljoin(url, picture_tag)
-        comments_tag = soup.select(".texts > .black")
-        comments = [comment.text for comment in comments_tag]
-        page = {
-            "title": book_details[0],
-            "author": book_details[1],
-            "picture_url": picture_url,
-            "comments": comments,
-        }
-        print(page["title"])
-        print(page["author"])
-        print(page["picture_url"])
-        print(page["comments"])
-
-
-
-
-print(parse_page(5, 6))
+def parse_page(url: str) -> dict:
+    """
+    Function for parsing data from the site.
+    Args:
+        url (str): Link to the site you want to parse.
+    returns:
+        page (dict): Keys: "title", "author", "picture_url", "comments", "genres".
+    """
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    book_details_tag = soup.find("h1")
+    book_details = list(map(lambda x: x.strip(), book_details_tag.text.split("::")))
+    picture_tag = soup.find("div", class_="bookimage").find("img")["src"]
+    picture_url = urljoin(url, picture_tag)
+    comments_tag = soup.select(".texts > .black")
+    comments = [comment.text for comment in comments_tag]
+    genres_tag = soup.find("span", class_="d_book").find_all("a")
+    genres = [genre.get_text() for genre in genres_tag]
+    page = {
+        "title": book_details[0],
+        "author": book_details[1],
+        "picture_url": picture_url,
+        "comments": comments,
+        "genres": genres,
+    }
+    return page
