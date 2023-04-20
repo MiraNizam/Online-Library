b0848e620025b1fbf153d58d70c5e86a0ec1f079
MiraNizam <82273561+MiraNizam@users.noreply.github.com>
2023-04-20 18:22:21 +0300

    Create HTML page with books

--- render_website.py
+++ render_website.py
@@ -1,14 +1,24 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+import json
+
 
 env = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html', 'xml'])
 )
 
-template = env.get_template('index.html')
+template = env.get_template('template.html')
+
+
+def get_book_descriptions():
+    with open("book_descriptions.json", "r") as file:
+        book_descriptions_json = file.read()
+    book_descriptions = json.loads(book_descriptions_json)
+    return book_descriptions
+
 
 rendered_page = template.render(
-    book_title="Бал хищников",
+    book_descriptions=get_book_descriptions(),
 )
 
 with open('rendered_index.html', 'w', encoding="utf8") as file:
