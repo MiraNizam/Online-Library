57cb4f95f004fb3b47408fc56523c82810a89f16
MiraNizam <82273561+MiraNizam@users.noreply.github.com>
2023-05-16 19:30:33 +0300

    Split books list by pages

--- render_website.py
+++ render_website.py
@@ -2,27 +2,31 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json
 from livereload import Server, shell
 from more_itertools import chunked
+import os
 
 
 def on_reload():
+    with open("book_descriptions.json", "r") as file:
+        book_descriptions_json = file.read()
+    book_descriptions = json.loads(book_descriptions_json)
+    chunked_descriptions_by_pages = list(chunked(book_descriptions, 20))
+
+    path = "pages"
+    os.makedirs(path, exist_ok=True)
+
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
     template = env.get_template('template.html')
-    rendered_page = template.render(
-        book_descriptions=get_book_descriptions(),
-    )
 
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+    for number, page in enumerate(chunked_descriptions_by_pages, 1):
+        page_path = os.path.join(path, f"index{number}.html")
+        rendered_page = template.render(book_descriptions=page)
 
+        with open(page_path, "w", encoding="utf-8") as file:
+            file.write(rendered_page)
 
-def get_book_descriptions():
-    with open("book_descriptions.json", "r") as file:
-        book_descriptions_json = file.read()
-    book_descriptions = json.loads(book_descriptions_json)
-    return book_descriptions
 
 
 def main():
@@ -33,5 +37,5 @@ def main():
 
 main()
 
-# if __name__ == "__main":
-#     main()
+if __name__ == "__main":
+    main()
